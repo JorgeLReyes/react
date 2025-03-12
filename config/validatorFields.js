d2c09d0075b1c0282b9298b917c6f6fb7be8fbb4
JorgeLReyes <JorgeReyes.130101@gmail.com>
2025-03-12 00:23:11 -0600

    Probando despliegue

--- config/validatorFields.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validatorRegex = void 0;
-exports.validatorRegex = {
-    email: /^[a-z0-9]+((_|\.)[a-z0-9]+)?@[a-z]+\.[a-z]+$/i,
-};
