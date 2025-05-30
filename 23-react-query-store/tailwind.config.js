bf56d6260b8828513934f802f9d8c93bad464d55
JorgeLReyes <JorgeReyes.130101@gmail.com>
2025-05-30 00:12:28 -0600

    Actualizando paquete a heroui

--- 23-react-query-store/tailwind.config.js
+++ 23-react-query-store/tailwind.config.js
@@ -1,17 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-// tailwind.config.js
-// eslint-disable-next-line
-const {nextui} = require("@nextui-org/react");
-
-
 export default {
   content: [
     "./src/**/*.{html,js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/react/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [],
 };
